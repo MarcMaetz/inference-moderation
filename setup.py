c9fe6cef9ed32efe5747e838f3888fe34ba66261
Marc Maetz <marc.maetz@proton.me>
2026-03-30 09:05:06 +0200

    edit name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="lakera",
+    name="inference-moderation",
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
